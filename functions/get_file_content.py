dae29d4cbfe93d78af1a9ca84d2a21086253e802
Michael <michaellittle92@gmail.com>
2025-08-09 15:04:44 +1000

    reading files

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+import os 
+from functions.config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+
+    full_path = os.path.abspath(os.path.join(working_directory,file_path))
+    working_directory_abs = os.path.abspath(working_directory)
+
+    if not full_path.startswith(working_directory_abs):
+        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(full_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+
+    try:
+        with open(full_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS + 1)
+        if len(file_content_string) == MAX_CHARS + 1:
+            file_content_string = file_content_string[:MAX_CHARS]
+            file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
+        return file_content_string
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,25 +1,5 @@
-from functions.get_files_info import get_files_info
-
-test_out = get_files_info("calculator",".")
-expected_out = ''' - main.py: file_size=576 bytes, is_dir=False
- - tests.py: file_size=1343 bytes, is_dir=False
- - pkg: file_size=92 bytes, is_dir=True
-'''
-print(test_out)
-
-test_out = get_files_info("calculator", "pkg")
-expected_out = '''
- - calculator.py: file_size=1739 bytes, is_dir=False
- - render.py: file_size=768 bytes, is_dir=False
-'''
-print(test_out)
-
-print("Result for '/bin' directory:")
-
-test_out = get_files_info("calculator", "/bin")
-expected_out = '''Error: Cannot list "/bin" as it is outside the permitted working directory'''
-print(test_out)
-print("Result for '../' directory:")
-test_out = get_files_info("calculator", "../")
-expected_out = '''Error: Cannot list "../" as it is outside the permitted working directory'''
-print(test_out)
+from functions.get_file_content import get_file_content
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
